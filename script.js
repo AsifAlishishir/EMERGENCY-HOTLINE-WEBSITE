136e537dba483ea6ba27672e6c64a9c891167044
AsifAlishishir <asifali210084@diit.edu.bd>
2026-02-23 15:57:04 +0600

    added call button functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const callHistoryData = [];
+
 // heart count functionality
 const heartBtns = document.querySelectorAll(".small-heart-btn");
 
@@ -10,9 +12,28 @@ for (const btn of heartBtns) {
   });
 }
 
-// copy button functionality
+// call button functionality
 const cardSection = document.getElementsByClassName("card");
-// console.log(cardSection);
+let coinTotal = parseInt(document.getElementById("coin-total").innerText);
+
+for (const card of cardSection) {
+  const cardTitle = card.querySelector("h2").innerText;
+  const cardNumber = card.querySelector("h3").innerText;
+  const callBtn = card.querySelector(".call-button");
+  callBtn.addEventListener("click", function () {
+    if (coinTotal < 20) {
+      alert("❌ insufficient Coin! You need 20 Coin to make a call.");
+      return;
+    }
+
+    alert("📞 Calling " + cardTitle + " - " + cardNumber + " ........");
+
+    coinTotal -= 20;
+    document.getElementById("coin-total").innerText = coinTotal;
+  });
+}
+
+// copy button functionality
 let copyTotal = parseInt(document.getElementById("copy-total").innerText);
 
 for (const card of cardSection) {
@@ -22,7 +43,7 @@ for (const card of cardSection) {
 
   copyBtn.addEventListener("click", function () {
     navigator.clipboard.writeText(cardNumber);
-    alert(cardTitle + " number has been copied!");
+    alert("✅ " + cardTitle + " number has been copied!");
     copyTotal++;
     document.getElementById("copy-total").innerText = copyTotal;
   });
